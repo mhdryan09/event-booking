20c39e71e7614fe88f9ce7d1115849abf41d1dff
Muhammad Ryan Pranata <pranataryan91@gmail.com>
2024-12-05 07:02:36 +0700

    Rendering a List of Components

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,3 +1,7 @@
 <template>
-  <h1>Event Card</h1>
+  <div class="bg-white border border-gray-200 rounded-md">
+    <h3 class="p-4 text-xl font-medium border-b border-gray-200">What</h3>
+    <p class="p-4 border-b border-gray-200">When</p>
+    <p class="p-4">Description</p>
+  </div>
 </template>
