4ee06a43659dd5c7b0a7bb95620885ec2f6f6546
Muhammad Ryan Pranata <pranataryan91@gmail.com>
2024-12-05 07:28:14 +0700

    Fall trough Attribute

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -3,13 +3,20 @@
     <h3 class="p-4 text-xl font-medium border-b border-gray-200">{{ title }}</h3>
     <p class="p-4 border-b border-gray-200">{{ when }}</p>
     <p class="p-4">{{ description }}</p>
+
+    <section class="flex justify-end p-4">
+      <Button label="Register" @click="$emit('register')" />
+    </section>
   </div>
 </template>
 
 <script setup>
+import Button from './Button.vue';
 defineProps({
   title: String,
   when: String,
   description: String
 });
+
+defineEmits(['register']);
 </script>
