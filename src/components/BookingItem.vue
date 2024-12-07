41b3b23ec32a1fd3e7b129105de0dbbe84d2bb1f
Muhammad Ryan Pranata <pranataryan91@gmail.com>
2024-12-07 17:58:44 +0700

    Component Variants

--- src/components/BookingItem.vue
+++ src/components/BookingItem.vue
@@ -1,7 +1,13 @@
 <template>
-  <SectionCard> Booking to Vue Conference </SectionCard>
+  <SectionCard>
+    <div class="flex justify-between">
+      Booking to Vue Conference
+      <RoundButton variant="danger">Cancel</RoundButton>
+    </div>
+  </SectionCard>
 </template>
 
 <script setup>
+import RoundButton from './RoundButton.vue';
 import SectionCard from './SectionCard.vue';
 </script>
--- src/components/RoundButton.vue
+++ src/components/RoundButton.vue
@@ -1,6 +1,9 @@
 <template>
   <button
-    class="px-3 py-1 text-sm font-medium border border-gray-200 rounded-full :hover:bg-gray-100"
+    :class="[
+      'px-3 py-1 text-sm font-medium border border-gray-200 rounded-full :hover:bg-gray-100',
+      variants[variant] ?? variants.default
+    ]"
     @click="$emit('click')"
   >
     <slot></slot>
@@ -8,5 +11,16 @@
 </template>
 
 <script setup>
+const variants = {
+  default: 'border border-gray-200 :hover:bg-gray-100',
+  danger: 'bg-red-200 enabled:hover:bg-red-100 text-red-900'
+};
+
+defineProps({
+  variant: {
+    type: String,
+    default: 'default'
+  }
+});
 defineEmits(['click']);
 </script>
